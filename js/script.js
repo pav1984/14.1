dc852e442cde3ae728f52bf9867a29932f5e34f8
Pawel Oczkowski <pawel.oczko84@gmail.com>
2018-12-05 16:51:40 +0100

    Add project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    hash: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity('.main-carousel', {
+    // options
+});
+
+document.querySelector("#btnStart").addEventListener('click', function () {
+    flkty.select(0);
+})
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
