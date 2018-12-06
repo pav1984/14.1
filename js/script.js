a4282389c4e3a50a34be8700fd18b3c14638d9f5
Pawel Oczkowski <pawel.oczko84@gmail.com>
2018-12-06 18:38:36 +0100

    Add connect flickity and maps

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
+//JS MUSTACHE
 var slideItem = document.getElementById('slide-item').innerHTML;
 Mustache.parse(slideItem);
 var listItems = '';
-
 for (var i = 0; i < mySlides.length; i++) {
-
     listItems += Mustache.render(slideItem, mySlides[i]);
-};
-
+}
 myCarousel.insertAdjacentHTML('beforeend', listItems);
-
-
-
-
-
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     // options
@@ -20,18 +13,17 @@ var flkty = new Flickity(elem, {
     contain: true,
     pageDots: false,
     hash: true,
-
 });
-
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity('.main-carousel', {
     // options
 });
-
+//RESTART BUTTON
 document.querySelector("#btnStart").addEventListener('click', function () {
     flkty.select(0);
 })
+//PROGRESS BAR
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on('scroll', function (progress) {
@@ -39,23 +31,88 @@ flkty.on('scroll', function (progress) {
     progressBar.style.width = progress * 100 + '%';
 });
 
+//MAPS+CAROUSEL
 
-//Maps
-
-var infos = document.getElementById('infos');
 window.initMap = function () {
-
   var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 1,
       center: mySlides[0].coords,
   });
-
     for (var i = 0; i < mySlides.length; i++) {
         var marker = new google.maps.Marker({
             position: mySlides[i].coords,
-            map: map,
-           
+            map: map,           
         });
-    }  
+          function slide (i) {
+              marker.addListener('click', function () {
+                  flkty.select(i);
+              });
+          } 
+          slide(i); 
+    }
+//FLICKITY ONCHANGE
+
+flkty.on('change', function (index) {
+   smoothPanAndZoom(map,8,mySlides[index].coords) 
+});
+
+//FUNKCJE 
+
+var smoothPanAndZoom = function (map, zoom, coords) {
+    var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+    jumpZoom = Math.min(jumpZoom, zoom - 1);
+    jumpZoom = Math.max(jumpZoom, 3);
+    smoothZoom(map, jumpZoom, function () {
+        smoothPan(map, coords, function () {
+            smoothZoom(map, zoom);
+        });
+    });
+};
+var smoothZoom = function (map, zoom, callback) {
+    var startingZoom = map.getZoom();
+    var steps = Math.abs(startingZoom - zoom);
+
+    if (!steps) { 
+        if (callback) {         
+            callback();
+        }
+        return;
+    }
+    var stepChange = -(startingZoom - zoom) / steps;
+    var i = 0;
+    var timer = window.setInterval(function () { 
+        if (++i >= steps) {         
+            window.clearInterval(timer);        
+            if (callback) {              
+                callback();
+            }
+        }      
+        map.setZoom(Math.round(startingZoom + stepChange * i));
+    }, 80);
+};
+
+var smoothPan = function (map, coords, callback) {
+var mapCenter = map.getCenter();
+coords = new google.maps.LatLng(coords);
+
+var steps = 12;
+var panStep = {
+    lat: (coords.lat() - mapCenter.lat()) / steps,
+    lng: (coords.lng() - mapCenter.lng()) / steps
+};
+
+var i = 0;
+var timer = window.setInterval(function () {
+    if (++i >= steps) {
+        window.clearInterval(timer);
+        if (callback) callback();
+    }
+    map.panTo({
+        lat: mapCenter.lat() + panStep.lat * i,
+        lng: mapCenter.lng() + panStep.lng * i
+    });
+}, 1000 / 30);
+};
+
 }
 
